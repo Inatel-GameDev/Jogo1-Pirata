12f52cbb8b8cc85a6061af574526861cc6eab857
dju4mota <fernandomotajunior2@gmail.com>
2024-03-06 22:38:59 -0300

    scene managent

---
+++ Jogo1/Assets/scripts/GameManager_Menu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_Menu : MonoBehaviour
+{
+    public void load_next_level()
+    {
+        
+    }
+}
